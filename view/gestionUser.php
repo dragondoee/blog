93d4c4c1ca12c3f1e22bb2faeb3d591f35a8d6f9
Meylie <emilie.desgranges78@gmail.com>
2024-11-04 15:02:23 +0100

    Mise à jour Fichiers
    Supression des fichiers Gestion
    Ajout manuel de photo de profil

--- view/gestionUser.php
+++ view/gestionUser.php
@@ -1,6 +1,6 @@
 <?php
 foreach ($users as $user) {
-
+    echo "<img src='img/profil/{$user["login"]}' alt='photo de profil de {$user["login"]}'>";
     echo "{$user["login"]}";
     if ($user["login"] != "admin") {
         echo "<a href='index.php?action=gestionUser&gestion=user&supr={$user["login"]}'> Supprimer </a><br>";
