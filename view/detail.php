3b5b777b0a40f136d36b6bc4fb97bce03a5447a8
Meylie <emilie.desgranges78@gmail.com>
2024-11-29 22:24:07 +0100

    bouton retourc

--- view/detail.php
+++ view/detail.php
@@ -1,10 +1,10 @@
+<a class="retour" href="index.php">retour</a>
 <div class="contenu">
-
     <div>
         <h1 id="content"><?= $billet["titre"]; ?></h1>
         <p class="date">publié le <?= (new DateTime($billet["date"]))->format('d/m/Y \à H\hi'); ?></p>
     </div>
-    <p class="description"><?= $billet["text"]; ?></p>
+    <p class="description"><?= nl2br($billet["text"]); ?></p>
 
 </div>
 <h2 class="h2-com">Commentaires : </h2>
